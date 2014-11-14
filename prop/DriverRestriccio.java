619791b12e8affadc1451c83f3a885197cb78cf3
Xisco <xisco.bosch.4a@gmail.com>
2014-11-14 01:24:33 +0100

    Driver de restricció amb herencia de classes. A sa classe Restriccio hi ha 2 llocs on he introduit de manera "fraudulenta" es torns perque quan es crea un arbre les pugui agafar sense necessitat de crear abans es calendari i tot això. Es un segon canviar-ho (esta com a comentari lo que hauria de ser es codi correcte). Aquesta part comentada de Restriccio fa refernència a sa cerca de torns dins calendari, es un mètode que he posat a Cjt_calendaris pero per si acas trobau que no esta be o esteis tocant es codi, no el pos a github encare. Com que es metode esta fet, si trobau que ha d'estar a un altre lloc, només es canviar es nom de sa crida desde Restriccio.

---
+++ prop/DriverRestriccio.java
@@ -0,0 +1,224 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prop;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Xisco
+ */
+public class DriverRestriccio {
+
+    public static void main(String[] args) throws Error {
+        Restriccio r = null;
+        String expressio = null;
+        Scanner scan;
+        while (true) {
+            System.out.println("---------MENU---------");
+            System.out.println("1 => Insereix una expressió per la restriccio");
+            System.out.println("2 => Crea l'arbre de la restriccio");
+            System.out.println("3 => Consulta l'operació de la restricció");
+            System.out.println("4 => Mostra l'arbre que sorgeix de la restricció");
+            System.out.println("5 => consulta el primer fill de la restricció");
+            System.out.println("6 => consulta el segon fill de la restricció");
+            System.out.println("7 => accedeix a la primera restricció filla");
+            System.out.println("8 => accedeix a la segona restricció filla");
+
+            scan = new Scanner(System.in);
+
+            int opcio = scan.nextInt();
+            switch (opcio) {
+                case 1:
+                    System.out.println("\nInsereix l'expressió que expresa la restricció:");
+                    scan = new Scanner(System.in);
+                    expressio = scan.nextLine();
+                    break;
+                case 2:
+                    try {
+                        r = Restriccio.crea_arbre(expressio);
+                    } catch (Error e) {
+                        System.err.println("Hi ha hagut un error durant la creació de l'arbre\n " + e);
+                    }
+                    break;
+                case 3:
+                    System.out.println(r.getOp());
+                    break;
+                case 4:
+                    MostraArbre(r);
+                    break;
+                case 5:
+                    String op = r.getOp();
+                    switch (op) {
+                        case "AND":
+                            R_AND and = (R_AND) r;
+                            Object oAnd = and.getFill1();
+                            if(oAnd.getClass().equals(Torn.class)){
+                                Torn f1 = (Torn) oAnd;
+                                System.out.println("El primer torn va de " + f1.getData_inici().getTime() + " fins " + f1.getData_fi().getTime());
+                            }
+                            else{
+                                Restriccio f1 = (Restriccio) oAnd;
+                                System.out.println("La primera restricció es " + f1.getOp());
+                            }
+                            break;
+                        case "OR":
+                            R_OR or = (R_OR) r;
+                            Object oOr = or.getFill1();
+                            if(Torn.class.equals(oOr.getClass())){
+                                Torn f1 = (Torn) oOr;
+                                System.out.println("El primer torn va de " + f1.getData_inici().getTime() + " fins " + f1.getData_fi().getTime());
+                            }
+                            else{
+                                Restriccio f1 = (Restriccio) oOr;
+                                System.out.println("La primera restricció es " + f1.getOp());
+                            }
+                            break;
+                        case "NOT":
+                            R_NOT not = (R_NOT) r;
+                            Object oNot = not.getFill();
+                            if(Torn.class.equals(oNot.getClass())){
+                                Torn f1 = (Torn) oNot;
+                                System.out.println("El primer torn va de " + f1.getData_inici().getTime() + " fins " + f1.getData_fi().getTime());
+                            }
+                            else{
+                                Restriccio f1 = (Restriccio) oNot;
+                                System.out.println("La primera restricció es " + f1.getOp());
+                            }
+                            break;
+                        case "NOP":
+                            R_NOP nop = (R_NOP) r;
+                            Torn nopt1 = nop.getTorn();
+                            System.out.println("El primer torn va de " + nopt1.getData_inici().getTime() + " fins " + nopt1.getData_fi().getTime());
+                            break;
+                    }
+                    break;
+                case 6:
+                    String op2 = r.getOp();
+                    switch (op2) {
+                        case "AND":
+                            R_AND and = (R_AND) r;
+                            Object oAnd = and.getFill2();
+                            if(oAnd.getClass().equals(Torn.class)){
+                                Torn f2 = (Torn) oAnd;
+                                System.out.println("El segon torn va de " + f2.getData_inici().getTime() + " fins " + f2.getData_fi().getTime());
+                            }
+                            else{
+                                Restriccio f2 = (Restriccio) oAnd;
+                                System.out.println("La segona restricció es " + f2.getOp());
+                            }
+                            break;
+                        case "OR":
+                            R_OR or = (R_OR) r;
+                            Object oOr = or.getFill2();
+                            if(Torn.class.equals(oOr.getClass())){
+                                Torn f2 = (Torn) oOr;
+                                System.out.println("El segon torn va de " + f2.getData_inici().getTime() + " fins " + f2.getData_fi().getTime());
+                            }
+                            else{
+                                Restriccio f2 = (Restriccio) oOr;
+                                System.out.println("La segona restricció es " + f2.getOp());
+                            }
+                            break;
+                    }
+                    break;
+                case 7:
+                    String op5 = r.getOp();
+                    switch (op5) {
+                        case "AND":
+                            R_AND and = (R_AND) r;
+                            if(!Torn.class.equals(and.getFill1().getClass())){
+                                r = (Restriccio) and.getFill1();
+                            }
+                            else{
+                                System.err.println("el fill no es una restriccio");
+                            }
+                            break;
+                        case "OR":
+                            R_OR or = (R_OR) r;
+                            if(!Torn.class.equals(or.getFill1().getClass())){
+                                r = (Restriccio) or.getFill1();
+                            }
+                            else{
+                                System.err.println("el fill no es una restriccio");
+                            }
+                            break;
+                        case "NOT":
+                            R_NOT not = (R_NOT) r;
+                            if(!Torn.class.equals(not.getFill().getClass())){
+                                r = (Restriccio) not.getFill();
+                            }
+                            else{
+                                System.err.println("el fill no es una restriccio");
+                            }
+                            break;
+                    }
+                    break;
+                case 8:
+                    String op6 = r.getOp();
+                    switch (op6) {
+                        case "AND":
+                            R_AND and = (R_AND) r;
+                            if(!Torn.class.equals(and.getFill2().getClass())){
+                                r = (Restriccio) and.getFill2();
+                            }
+                            else{
+                                System.err.println("el fill no es una restriccio");
+                            }
+                            break;
+                        case "OR":
+                            R_OR or = (R_OR) r;
+                            if(!Torn.class.equals(or.getFill2().getClass())){
+                                r = (Restriccio) or.getFill2();
+                            }
+                            else{
+                                System.err.println("el fill no es una restriccio");
+                            }
+                            break;
+                    }
+                    break;
+            }
+
+        }
+    }
+
+    private static void MostraArbre(Object o) throws Error {
+        if (R_AND.class.equals(o.getClass())
+                || R_OR.class.equals(o.getClass())
+                || R_NOT.class.equals(o.getClass())
+                || R_NOP.class.equals(o.getClass())) {
+            Restriccio r = (Restriccio) o;
+            String op = r.getOp();
+            switch (op) {
+                case "AND":
+                    R_AND and = (R_AND) r;
+                    System.out.print("(");
+                    MostraArbre(and.getFill1());
+                    System.out.print(" , ");
+                    MostraArbre(and.getFill2());
+                    break;
+                case "OR":
+                    R_OR or = (R_OR) r;
+                    System.out.print("(");
+                    MostraArbre(or.getFill1());
+                    System.out.print(" , ");
+                    MostraArbre(or.getFill2());
+                    break;
+                case "NOT":
+                    R_NOT not = (R_NOT) r;
+                    System.out.print("(");
+                    MostraArbre(not.getFill());
+                    break;
+                case "NOP":
+                    R_NOP nop = (R_NOP) r;
+                    System.out.print("( T ");
+                    break;
+            }
+            System.out.print(")-->" + op + "\n");
+        }  else {
+            System.out.print(" T ");
+        }
+    }
+}
--- prop/Restriccio.java
+++ prop/Restriccio.java
@@ -4,14 +4,9 @@
  */
 package prop;
 
-<<<<<<< HEAD
-public abstract class Restriccio {
-=======
-import prop.Operacio_Logica.OP;
+import java.util.GregorianCalendar;
 
-public class Restriccio {
-    private OP op;
->>>>>>> 866e95ac90529616f1234859b9902c1d518682d7
+public abstract class Restriccio {
     private Torn t1 = null, t2 = null;
     private Restriccio r1, r2;
     private Doctor d; 
@@ -22,143 +17,6 @@ public class Restriccio {
     //////////////////////////////////////////////////////////////////////////
     
     /**
-<<<<<<< HEAD
-=======
-     * Restricció formada per una operació lògica i per un puntrer a un torn
-     * @param op Operació lògica
-     * @param t Torn fill
-     * @throws Error 
-     */
-    public Restriccio(OP op, Torn t) throws Error{
-        if(op == OP.NOT || op == OP.NOP){
-            this.op = op;
-            this.t1 = t;
-        }
-        else{
-            throw new Error("No es pot utilitzar la operacio AND o OR a aquesta restriccio");
-        }
-        
-    }
-    
-    /**
-     * Restriccio composada per una operació lògica i un punter a una altra restricció
-     * @param op Operació lògica
-     * @param r1 Restricció filla
-     * @throws Error 
-     */
-    private Restriccio(OP op, Restriccio r1) throws Error{
-        if(op == OP.NOT || op == OP.NOP){
-            this.op = op;
-            this.r1 = r1;
-        }
-        else{
-            throw new Error("No es pot utilitzar la operacio AND o OR a aquesta restriccio");
-        }
-    }
-    
-    /**
-     * Restricció formada per una operació lògica i 2 punters a altres restriccions
-     * @param op Operació lògica
-     * @param r1 Preimera restricció filla 
-     * @param r2 Segona restricció filla
-     * @throws Error 
-     */
-    public Restriccio(OP op, Restriccio r1, Restriccio r2) throws Error{
-        if(op != OP.NOT || op != OP.NOP){
-            this.op = op;
-            this.r1 = r1;
-            this.r2 = r2;
-        }
-        else{
-            throw new Error("No es pot utilitzar la operacio NOT o NOP a aquesta restriccio");
-        }
-    }
-    
-    /**
-     * Restricció formada per una operació lògica i 2 punters a torns
-     * @param op Operació lògica
-     * @param t1 Primer torn fill
-     * @param t2 Segon torn fill
-     * @throws Error 
-     */
-    public Restriccio(OP op, Torn t1, Torn t2) throws Error{
-        if(op != OP.NOT || op != OP.NOP){
-            this.op = op;
-            this.t1 = t1;
-            this.t2 = t2;
-        }
-        else{
-            throw new Error("No es pot utilitzar la operacio NOT o NOP a aquesta restriccio");
-        }
-    }
-    
-// En principi no es necessita una comprovació de si es compleix o no una restricció ja que aquestes
-// formen part del graf i per tant (si es tradueix be de l'arbre al graf) sempre que es trobi una solució
-// s'haurien de complir totes les restriccions
-// De totes paneres, per si acas no elimin el codi, sols el coment
-//
-//    /**
-//     * Funció que retorna un boolea que permet determinar si s'ha complit aquesta restricció o no
-//     * @return boolea que determina si es compleix la restricció o no
-//     */
-//    public boolean compleixRestriccio(){
-//        boolean compleix = false;
-//        if(t1 == null && t2 == null && r2!=null){ //restriccio composada per 2 restriccions
-//            switch(op){
-//                case AND:
-//                    compleix = r1.compleixRestriccio() && r2.compleixRestriccio();
-//                break;
-//                
-//                case OR:
-//                    compleix = r1.compleixRestriccio() || r2.compleixRestriccio();
-//                break;
-//                    
-//                case XOR:
-//                    compleix = (r1.compleixRestriccio() && !r2.compleixRestriccio())
-//                                ||
-//                               (r2.compleixRestriccio() && !r1.compleixRestriccio());
-//                break;
-//                    
-//                case NAND:
-//                    compleix = !(r1.compleixRestriccio() && r2.compleixRestriccio());
-//                break;
-//            }
-//        }
-//        else if(r1 != null && r2 == null){ // restriccio composada per 1 resticcio
-//            if(op == OP.NOP) compleix = r1.compleixRestriccio(); 
-//            else compleix = !r1.compleixRestriccio();
-//        }
-//        else if (t1 != null && t2 == null){ // restriccio composada per 1 Torn
-//            if(op == OP.NOP) compleix = t1.assignat(); //revisar com es sabra si un torn esta disponible o no
-//            else compleix = !t1.assignat();
-//        }
-//        else if (t1 != null && t2 != null){ // restriccio composada per 2 torns
-//            switch(op){
-//                case AND:
-//                    compleix = t1.esDisponible() && t2.esDisponible();
-//                break;
-//                
-//                case OR:
-//                    compleix = t1.esDisponible() || t2.esDisponible();
-//                break;
-//                    
-//                case XOR:
-//                    compleix = (t1.esDisponible() && !t2.esDisponible())
-//                                ||
-//                               (t2.esDisponible() && !t1.esDisponible());
-//                break;
-//                    
-//                case NAND:
-//                    compleix = !(t1.esDisponible() && t2.esDisponible());
-//                break;
-//            }
-//        }
-//        
-//        return compleix;
-//    }
-    
-    /**
->>>>>>> 866e95ac90529616f1234859b9902c1d518682d7
      * Donat una explresió lògica, es crea un arbre que la representa i en retorna l'arrel
      * @param s String amb la explresió lògica que determina la restricció completa
      * @return Restricció arrel de l'arbre
@@ -173,7 +31,7 @@ public class Restriccio {
           s = s.substring(1); // treim el primer caracter de s
        }
        if (c.equals("(")) {
-           String sub_r1 = cerca_tancament(s);
+           String sub_r1 = cerca_tancament(s);;
            s = s.substring(sub_r1.length()); // treim la part de sub_r1 de s
            c = s.substring(0,1); // agafam el primer caracter de s
            s = s.substring(1); // treim el primer caracter de s
@@ -190,71 +48,40 @@ public class Restriccio {
            if (esRestriccio(sub_r1) && esRestriccio(sub_r2)){
                Restriccio r1 = crea_arbre(sub_r1);
                Restriccio r2 = crea_arbre(sub_r2);
-<<<<<<< HEAD
                switch (operacio){
                    case "AND":
                       r = new R_AND(r1,r2); 
                       break;
                    case "OR":
                        r = new R_OR(r1,r2);
-=======
-               OP op;
-               switch (operacio){
-                   case "AND":
-                      op = OP.AND; 
-                      break;
-                   case "NAND":
-                       op = OP.NAND;
-                       break;
-                   case "OR":
-                       op = OP.OR;
-                       break;
-                   case "XOR":
-                       op = OP.XOR;
->>>>>>> 866e95ac90529616f1234859b9902c1d518682d7
                        break;
                    default:
                        throw new Error("El nom de l'operació logica no es correcte");
                }
-<<<<<<< HEAD
            }
            else if(!esRestriccio(sub_r1) && !esRestriccio(sub_r2)){
-               Torn t1 = Cjt_calendaris.cerca_torn(sub_r1); //retorna el torn que te per identificador sub_r1
-               Torn t2 = Cjt_calendaris.cerca_torn(sub_r2); //retorna el torn que te per identificador sub_r2
+               //Torn t1 = Cjt_calendaris.cerca_torn(sub_r1); //retorna el torn que te per identificador sub_r1
+               //Torn t2 = Cjt_calendaris.cerca_torn(sub_r2); //retorna el torn que te per identificador sub_r2
+               GregorianCalendar d_i = new GregorianCalendar(1000, 10, 20, 10, 30);
+               GregorianCalendar d_f = new GregorianCalendar(1000, 10, 20, 11, 00);
+               Torn t1 = new Torn(d_i, d_f, 1, 10);
+               d_i = new GregorianCalendar(1000, 10, 20, 13, 00);
+               d_f = new GregorianCalendar(1000, 10, 20, 14, 00);
+               Torn t2 = new Torn(d_i, d_f, 2, 10);
+               
+               
+               
+               
                switch (operacio){
                    case "AND":
                       r = new R_AND(t1,t2);
                       break;
                    case "OR":
                        r = new R_OR(t1,t2);
-=======
-               r = new Restriccio(op,r1,r2);
-           }
-           else if(!esRestriccio(sub_r1) && !esRestriccio(sub_r2)){
-               Torn t1 = consulta_torn(sub_r1); //retorna el torn que te per identificador sub_r1
-               Torn t2 = consulta_torn(sub_r2); //retorna el torn que te per identificador sub_r2
-               OP op;
-               switch (operacio){
-                   case "AND":
-                      op = OP.AND; 
-                      break;
-                   case "NAND":
-                       op = OP.NAND;
-                       break;
-                   case "OR":
-                       op = OP.OR;
-                       break;
-                   case "XOR":
-                       op = OP.XOR;
->>>>>>> 866e95ac90529616f1234859b9902c1d518682d7
                        break;
                    default:
                        throw new Error("El nom de l'operació logica no es correcte");
                }
-<<<<<<< HEAD
-=======
-               r = new Restriccio(op,t1,t2);
->>>>>>> 866e95ac90529616f1234859b9902c1d518682d7
            }
            else{
                throw new Error("Error a la deteccio dels parametres de les restriccions");
@@ -263,10 +90,9 @@ public class Restriccio {
        else if( c.equals("N")){ //es NOT o NOP
            s = c+s; // Afagim el caracter a l'inici de la cadena per treure l'operacio sencera
            String operacio = llegeix_op(s);
-           s = s.substring(s.length());
+           s = s.substring(operacio.length());
            c = s.substring(0,1);
            s = s.substring(1);
-<<<<<<< HEAD
            String sub_r1 = cerca_tancament(s);
            if (esRestriccio(sub_r1)){
                Restriccio r1 = crea_arbre(sub_r1);
@@ -274,43 +100,22 @@ public class Restriccio {
                r = new R_NOT(r1);
            }
            else{
-               Torn t1 = Cjt_calendaris.cerca_torn(sub_r1);
+               //Torn t1 = Cjt_calendaris.cerca_torn(sub_r1);
+               GregorianCalendar d_i = new GregorianCalendar(1000, 10, 20, 10, 30);
+               GregorianCalendar d_f = new GregorianCalendar(1000, 10, 20, 11, 00);
+               Torn t1 = new Torn(d_i, d_f, 1, 10);
+               
+               
                switch (operacio){
                    case "NOT":
                       r = new R_NOT(t1);
                       break;
                    case "NOP":
                        r = new R_NOP(t1);
-=======
-           OP op;
-           switch (operacio){
-                   case "AND":
-                      op = OP.AND; 
-                      break;
-                   case "NAND":
-                       op = OP.NAND;
-                       break;
-                   case "OR":
-                       op = OP.OR;
-                       break;
-                   case "XOR":
-                       op = OP.XOR;
->>>>>>> 866e95ac90529616f1234859b9902c1d518682d7
                        break;
                    default:
                        throw new Error("El nom de l'operació logica no es correcte");
                }
-<<<<<<< HEAD
-=======
-           String sub_r1 = cerca_tancament(s);
-           if (esRestriccio(sub_r1)){
-               Restriccio r1 = crea_arbre(sub_r1);
-               r = new Restriccio(op, r1);
-           }
-           else{
-               Torn t1 = consulta_torn(sub_r1);
-               r = new Restriccio(op, t1);
->>>>>>> 866e95ac90529616f1234859b9902c1d518682d7
            }
        }
        else {
@@ -325,8 +130,11 @@ public class Restriccio {
      * @return Substring que conté l'explresio encapsulada dins el primer parentesis de l'expresió
      */
     private static String cerca_tancament(String s){
-        String sub = null;
-        while(!s.substring(0, 1).equals(")")){
+        String sub = "";
+        int contador = 0; //parentesis que ha trobat oberts abans del primer tancat
+        while(!s.substring(0, 1).equals(")") || contador>0){
+            if(s.substring(0,1).equals("(")) contador++;
+            else if(s.substring(0, 1).equals(")"))contador--;
             sub += s.substring(0,1);
             s = s.substring(1);
         }
@@ -339,8 +147,8 @@ public class Restriccio {
      * @return Substring que conte el nom de la operacio lògica
      */
     private static String llegeix_op(String s){
-        String op = null;
-        while(!s.substring(0,1).equals(" ") || !s.substring(0,1).equals("(") || !s.substring(0,1).equals(")")){
+        String op = "";
+        while(!s.substring(0,1).equals(" ") && !s.substring(0,1).equals("(") && !s.substring(0,1).equals(")")){
             op += s.substring(0,1); 
             s = s.substring(1);
         }
@@ -360,8 +168,10 @@ public class Restriccio {
         if (s.subSequence(0,1).equals("(")){ // si troba un parentesi obert dona per suposat que es una restriccio del tipus (...)AND(...) o (...)OR(...)
             esR = true;
         }
-        else if (s.substring(0,3).equals("NOP") || s.substring(0,3).equals("NOT")){ // si llegeix NOT o NOP donara per suposat que es una restricció
-            esR = true;
+        else if (s.length()>3){
+            if (s.substring(0,3).equals("NOP") || s.substring(0,3).equals("NOT")){ // si llegeix NOT o NOP donara per suposat que es una restricció
+                esR = true;
+            }
         }
         // en qualsevol altre cas no es considerarà restreicció
         return esR;
@@ -371,46 +181,10 @@ public class Restriccio {
      * Retorna la primera restricció en cas de que la actual estigui composta per una o més restriccions
      * @return Primera restricció filla
      */
-<<<<<<< HEAD
-=======
-    public Restriccio getRestriccio1(){
-        return r1;
-    }
-    
-    /**
-     * Retorna la segona restricció en cas de que la actual estigui composta per més d'una restricció
-     * @return Segona restricció filla
-     */
-    public Restriccio getRestriccio2(){
-        return r2;
-    }
-    
-    /**
-     * Retorna el primer torn en cas de que la restricció actual estigui composta per un o més torns 
-     * @return Primer torn fill
-     */
-    public Torn getTorn1(){
-        return t1;
-    }
-    
-    /**
-     * Retorna el segon torn en cas de que la restricció actual estigui composta més d'un torn
-     * @return Segon torn fill
-     */
-    public Torn getTorn2(){
-        return t2;
-    }
->>>>>>> 866e95ac90529616f1234859b9902c1d518682d7
     
     /**
      * Retorna l'operació lògica que s'aplica a aquesta restricció
      * @return Operació lògica
      */
-<<<<<<< HEAD
     public abstract String getOp();
-=======
-    public OP getOp(){
-        return op;
-    }
->>>>>>> 866e95ac90529616f1234859b9902c1d518682d7
 }
