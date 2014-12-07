d68297275987573357b4c62f74c3955922867d57
Laz <lazeru08@gmail.com>
2014-12-07 21:21:30 +0100

    Ara ordena per Cognom1, Cognom2, Nom de Pila i DNI, en aquest ordre si els atributs son iguals (Acaba recorrent a dni si tots els noms coincideixen, ja que DNI es clau unica)

--- prop/compNom.java
+++ prop/compNom.java
@@ -7,8 +7,18 @@ public class compNom implements Comparator<Doctor> {
     @Override
     public int compare(Doctor doc1, Doctor doc2){
         if (doc1 == doc2) return 0;
-        if(doc1.getNom() == null) return 1;
-        if(doc2.getNom() == null) return -1;
-        return doc1.getNom().compareTo(doc2.getNom());
+        if (doc1.getCognom1().equals(doc2.getCognom1())){
+                if(doc1.getCognom2().equals(doc2.getCognom2())){
+                    if(doc1.getNom().equals(doc2.getNom())){
+                        //Ordenem per DNI si tenen el mateix nom, cognom1 i cognom2
+                        return doc1.getdni().compareTo(doc2.getdni());
+                    }
+                    return doc1.getNom().compareTo(doc2.getNom());
+                }
+                return doc1.getCognom2().compareTo(doc2.getCognom2());
+        }
+        if(doc1.getCognom1() == null) return 1;
+        if(doc2.getCognom1() == null) return -1;
+        return doc1.getCognom1().compareTo(doc2.getCognom1());
     }
 }
