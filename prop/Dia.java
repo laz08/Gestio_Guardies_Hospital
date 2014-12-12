1aa2ac1c57b4e64d873402b2831da952814f067b
carladivicuesta <carla.divi@est.fib.upc.edu>
2014-12-12 12:23:05 +0100

    Commit de Carla

--- prop/Dia.java
+++ prop/Dia.java
@@ -1,63 +1,71 @@
 package prop;
 
 public class Dia {
+	
 	private boolean festiu;
 	private Torn[] torns;
 	
 	
-	/**
-	 * Creadora de dia
-	 * @param f
-	 */
+	//Pre: -
+	//Post: creem un dia amb tres torns buits i el boolea de festiu com ens indiqui el parametre
 	public Dia(boolean f) {
 		festiu = f;
 		torns = new Torn[3];
 	}
 	
-	/**
-	 * Consultora de si el dia es festiu
-	 * @return
-	 */
+
+	//Pre: -
+	//Post: Retornem un boolea indicant si el dia es festiu o no
 	public boolean getFestiu() {
 		return festiu;
 	}
 	
-	/**
-	 * Modifica si el dia es festiu
-	 * @param b
-	 */
+	//Pre: -
+	//Post: Modifiquem el boolea de si el dia es festiu pel que ens passen per parametres
 	public void setFestiu(boolean b) {
 		festiu = b;
 	}
 	
-	
+	//Pre: -
+	//Post: Modifiquem un dels torns pel torn nou que ens arriba, segons els parametres modifiquem un o altre
 	public void setTorn(Torn t) {
 		if(t.getHora_inici()<9) torns[0]=t;
 		else if(t.getHora_inici()<17) torns[1]=t;
 		else if(t.getHora_inici()<24) torns[2]=t;
 	}
 	
+	
+	//Pre: El parametre i ha d'estar entre 0 i 2, i el Torn t ha de cumplir les condicions de les hores segons i.
+	//Post: Modifiquem el torn de mati, tarda o nit(segons la i) pel nou torn que ens arriba.
 	public void setTornConcret(Torn t, int i) {
 		torns[i]=t;
 	}
 	
-	/**
-	 * Consultora dels torns del dia
-	 * @return
-	 */
+	//Pre: -
+	//Post: Retornem els torns del dia
 	public Torn[] getTorns(){
 		return torns;
 	}
 	
-	/**
-	 * Consultora d'un torn del dia
-	 * @param i
-	 * @return
-	 */
+	//Pre: El parametre i ha d'estar entre 0 i 2.
+	//Post: Retornem el torn que es troba a la posició i.
 	public Torn getTorn_concret(int i) {
 		return torns[i];
 	}
 	
+	//Pre: -
+	//Post: Borrem tota la informacio del torn i
+	public void borrarTorn(int i) {
+		//torns[i].borrar();
+		torns = null;
+	}
 	
+	//Pre: -
+	//Post: Borrem tota la informació del dia
+	public void borrar() {
+		festiu = false;
+		torns = null;
+		//for(int i=0; i<3; ++i) torns[i].borrar();
+	}
 	
 }
--- prop/DriverCalendari.java
+++ prop/DriverCalendari.java
@@ -1,55 +1,45 @@
 package prop;
 
+import java.util.GregorianCalendar;
 import java.util.Scanner;
 
 public class DriverCalendari {
 	
-	private static Calendari c;
+	private static Calendari c = new Calendari(null,0,0);
+	static Scanner arg = new Scanner(System.in);
+	static boolean primer = true;	
 	
 	public static void main(String[] args) throws Error {
 		
-		Scanner arg;
 		int cas;
 		boolean sortir = false;
+		escriureMenu();
 		
-		while(!sortir) {
+		while(!sortir && arg.hasNext()) {
 			try {
-				
-				escriureMenu();
-				
-				arg = new Scanner(System.in);
 				cas = arg.nextInt();
-				
-				switch(cas) {
-				
-					case 0:
-						sortir = true;
-						break;
-						
-					case 1:
-						crearCalendari(arg);
-						break;
-						
-					case 2:
-						modificarPlantilla(arg);
-						break;
-						
-					case 3:
-						consultarCalendari();
-						break;
-						
-					case 4:
-						consultarPlantilla();
-						break;
-						
-					default:
-						System.out.println("El numero ha d'estar entre 0 i 2.");
-						break;
-				
-				}
-			} catch(Exception e) {
-				System.err.println("Has d'introduir un numero\n"+e);
+			} catch (Exception e) {
+				System.out.println("s'esperava la introducció d'un número");
+				System.out.print(">>");
+				arg.next();
+				continue;
 			}
+				
+			switch(cas) {
+				case 0: sortir = true; break;
+				case 1: crearCalendari(); break;
+				case 2: modificarPlantilla(); break;
+				case 3: consultarCalendari(); break;
+				case 4: consultarPlantilla(); break;
+				case 5: consultarDiaConcret(); break;
+				case 6: borrarDiaConcret(); break;
+				case 7: borrar(); break;
+				default:
+					System.out.println("El numero ha d'estar entre 0 i 7.");
+					break;
+				
+			}
+			escriureMenu();
 		}
 	}
 	
@@ -57,38 +47,96 @@ public class DriverCalendari {
 		System.out.println("\n\n----------Menú----------"
 				+ "1.- Crear Calendari(identificador plantilla: String, any inici: int, any fi: int)\n"
 				+ "--------------------------\n"
-				+ "2.- Modificar Calendari(id: int)"
 				+ "2.- Modificar Plantilla associada(identificador plantilla nova: String)\n"
 				+ "--------------------------\n"
 				+ "3.- Consultar Calendari\n"
-				+ "4.- Consultar Plantilla associada"
+				+ "4.- Consultar Plantilla associada\n"
+				+ "5.- Consultar dia concret (any, mes dia)\n"
+				+ "--------------------------\n"
+				+ "6.- Borrar dia concret (any, mes, dia)\n"
+				+ "7.- Borrar calendari\n"
 				+ "--------------------------\n"
 				+ "0.- Exit\n\n");
 	}
 	
-	public static void crearCalendari(Scanner s) {
-		String plt = s.next();
-		int any_i = s.nextInt();
-		int any_fi = s.nextInt();
+	public static void crearCalendari() {
+		String plt = arg.next();
+		int any_i = arg.nextInt();
+		int any_fi = arg.nextInt();
 		c = new Calendari(plt,any_i,any_fi);
 	}
 	
-	public static void modificarIdentificador(Scanner s) {
-		int id = s.nextInt();
+	/**public static void modificarIdentificador() {
+		int id = arg.nextInt();
 		c.setIdentificador(id);
-	}
+	}*/
 	
-	public static void modificarPlantilla(Scanner s) {
-		String pltilla = s.next();
+	public static void modificarPlantilla() {
+		String pltilla = arg.next();
 		c.setPlantillaAssociada(pltilla);
 	}
 	
 	public static void consultarCalendari() {
-		
+		for(int i=0; i<c.getCalendari().length; ++i){
+			Dia d = c.getCalendari()[i];
+			GregorianCalendar data = c.quinDia(i);
+			System.out.println("El dia "+data.getTime().toString());
+			if(d.getFestiu())System.out.println("Es festiu");
+			else System.out.println("No es festiu");
+			Torn t;
+			String s;
+			for(int j=0; j<3; ++j) {
+				t = d.getTorn_concret(j);
+				if(j==0) s="Mati";
+				else if(j==1) s="Tarda";
+				else s="Nit";
+				System.out.println("Torn de "+s+":\n"
+						+ "Hora inici: "+t.getHora_inici()+"\n"
+						+ "Hora fi: "+t.getHora_fi()+"\n"
+						+ "Percentatge de sou: "+t.getPercent_sou()+"\n"
+						+ "Numero minim de doctors: "+t.getMin_num_doctors()+"\n\n");
+			}
+		}
 	}
 	
 	public static void consultarPlantilla() {
 		System.out.println(c.getPlantillaAssociada());
+	}
+	
+	public static void consultarDiaConcret() {
+		int any = arg.nextInt();
+		int mes = arg.nextInt();
+		int dia = arg.nextInt();
+		GregorianCalendar data = new GregorianCalendar(any,mes,dia);
+		Dia d = c.consultarDiaConcret(data);
+		System.out.println("El dia "+dia+"/"+mes+"/"+any);
+		if(d.getFestiu())System.out.println("Es festiu");
+		else System.out.println("No es festiu");
+		Torn t;
+		String s;
+		for(int i=0; i<3; ++i) {
+			t = d.getTorn_concret(i);
+			if(i==0) s="Mati";
+			else if(i==1) s="Tarda";
+			else s="Nit";
+			System.out.println("Torn de "+s+":\n"
+					+ "Hora inici: "+t.getHora_inici()+"\n"
+					+ "Hora fi: "+t.getHora_fi()+"\n"
+					+ "Percentatge de sou: "+t.getPercent_sou()+"\n"
+					+ "Numero minim de doctors: "+t.getMin_num_doctors()+"\n\n");
+		}
+	}
+	
+	public static void borrarDiaConcret() {
+		int any = arg.nextInt();
+		int mes = arg.nextInt();
+		int dia = arg.nextInt();
+		GregorianCalendar data = new GregorianCalendar(any,mes,dia);
+		c.borrarDiaConcret(data);		
+	}
+	
+	public static void borrar() {
+		c.borrar();
 	}
 	
 }
