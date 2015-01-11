5a0dda29044f8288de17626b04dc32f9decc4668
charlyo <carlinyos18@gmail.com>
2015-01-11 19:33:17 +0100

    Canviades mides de la llista: min 250, max 600

--- prop/PanelLlistat.java
+++ prop/PanelLlistat.java
@@ -12,16 +12,16 @@ public class PanelLlistat extends Panel implements ListSelectionListener{
 	protected JList<String> llista1 = new JList<String>(model1);
 	protected JScrollPane scroll1 = new JScrollPane(llista1);
 	public PanelLlistat(){
-		setSize(350,600);
+		setMinimumSize(new Dimension(350,300));
+		setMaximumSize(new Dimension(350,600));
 		setLayout(new BorderLayout());
 		add(scroll1, BorderLayout.CENTER);
-        scroll1.setPreferredSize(new Dimension(350, 600));
+        scroll1.setMinimumSize(new Dimension(350, 250));
+        scroll1.setPreferredSize(new Dimension(350,250));
         scroll1.setMaximumSize(new Dimension(350, 600));
 	}
 
-	@Override
 	public void valueChanged(ListSelectionEvent arg0) {
-		// TODO Auto-generated method stub
 		
 	}
 }
